40df0a45e08913c0e5d1f23e2b071189cb5f9b8e
Omer Faruk APLAK <omeraplak@gmail.com>
2019-07-25 13:14:45 +0300

    add babel

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  import 'whatwg-fetch'
+
+  let data = [];
+
+  onMount(async function() {
+    const response = await window.fetch("https://academy.valentinog.com/api/link/");
+    const json = await response.json();
+    data = json;
+  });
+</script>
+
+<ul>
+  {#each data as link}
+    <li>
+      <a href={link.url}>{link.title}</a>
+    </li>
+  {/each}
+</ul>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "@babel/polyfill";
+
+
+
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'world2'
+	}
+});
+
+window.app = app;
+
+export default app;
