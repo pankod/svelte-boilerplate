bf5f146fd31b476efc04c25316c26a3db6bdb5bc
Atakan Ermiş <ataerms@gmail.com>
2019-08-02 15:42:52 +0300

    ESLint and Prettier support (#5)
    * ESLint and Prettier support

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: "all",
+    pluginSearchDirs: ["src"],
+    plugins: ["prettier-plugin-svelte"],
+};
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,10 @@
 <script>
-  import Layout from "@Components/Layout";
-  import HelloWorld from "@Components/HelloWorld";
-  import FetchExample from "@Components/FetchExample";
-
+    import Layout from '@Components/Layout';
+    import HelloWorld from '@Components/HelloWorld';
+    import FetchExample from '@Components/FetchExample';
 </script>
 
 <Layout>
-  <HelloWorld />
-  <FetchExample />
+    <HelloWorld />
+    <FetchExample />
 </Layout>
