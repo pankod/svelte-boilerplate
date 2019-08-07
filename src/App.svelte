b58ca572b2e947eb4312a9a751a742fc4ca7cb40
aydın akyol <aydinakyol11@gmail.com>
2019-08-07 10:51:35 +0300

    pankod opensource png added

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
     import Layout from '@Components/Layout';
     import HelloWorld from '@Components/HelloWorld';
     import FetchExample from '@Components/FetchExample';
+    import CustomImage from '@Components/CustomImage';
 </script>
 
 <Layout>
     <HelloWorld />
     <FetchExample />
+    <CustomImage />
 </Layout>
