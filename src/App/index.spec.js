9ea172a47755729df2b6bc443c938e037a86146f
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-27 16:55:00 +0300

    Fix specs

--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -9,10 +9,12 @@ describe('App', () => {
         expect(container.getElementsByClassName('image-content').length).toBe(1)
         expect(container.getElementsByClassName('bg-image').length).toBe(1)
         expect(container.getElementsByClassName('title').length).toBe(1)
+        expect(container.getElementsByClassName('title__top').length).toBe(1)
         expect(container.getElementsByClassName('title__back').length).toBe(1)
         expect(container.getElementsByClassName('title__front').length).toBe(1)
 
-        expect(getAllByText('Hello World!').length).toBe(2)
+        expect(getAllByText('Hello').length).toBe(1)
+        expect(getAllByText('World').length).toBe(2)
 
         expect(container).toMatchSnapshot()
     })
--- src/Components/HelloWorld/index.spec.js
+++ src/Components/HelloWorld/index.spec.js
@@ -5,10 +5,14 @@ describe('Hello World', () => {
     it('renders Hello World! text', () => {
         const { container, getAllByText } = render(HelloWorld)
 
-        expect(getAllByText('Hello World!').length).toBe(2)
+        expect(getAllByText('Hello').length).toBe(1)
+        expect(getAllByText('World').length).toBe(2)
+
         expect(container.getElementsByClassName('title').length).toBe(1)
-        expect(container.getElementsByClassName('title__back').length).toBe(1)
+        expect(container.getElementsByClassName('title__top').length).toBe(1)
         expect(container.getElementsByClassName('title__front').length).toBe(1)
+        expect(container.getElementsByClassName('title__back').length).toBe(1)
+
         expect(container).toMatchSnapshot()
     });
 });
