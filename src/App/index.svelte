811a9b3517198aa5a1b2cade92930032853cc7a8
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-27 12:17:55 +0300

    Styling for App, move App.svelte to /App folder

---
+++ src/App/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Layout from '@Components/Layout';
+    import ImageExample from '@Components/ImageExample';
+    import HelloWorld from '@Components/HelloWorld';
+    import '../Assets/css/global.css';
+</script>
+
+<style src="./style.scss">
+
+</style>
+
+<Layout>
+    <div class="container">
+        <div class="container__top">
+            <ImageExample />
+        </div>
+        <div class="container__middle">
+            <HelloWorld />
+        </div>
+    </div>
+</Layout>
