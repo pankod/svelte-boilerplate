ff90a4542161cc8547d378707e02848b3257a1f5
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-27 12:19:29 +0300

    Update App.svelte reference and remove unnecessary props from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import "@babel/polyfill";
 
-import App from './App.svelte';
+import App from './App/index.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world2'
-	}
+    target: document.body
 });
 
 window.app = app;
