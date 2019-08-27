e9f1f205af5ca49c21fe3c1468116b6263602e76
Batuhan Ozgur Ozdemir <batuhanwilhelm@gmail.com>
2019-08-27 16:20:28 +0300

    Update App spec since FetchExample removed

--- src/App/index.spec.js
+++ src/App/index.spec.js
@@ -1,25 +1,19 @@
-import App from './App.svelte'
+import App from './index.svelte'
 import { render } from '@testing-library/svelte'
 
 describe('App', () => {
-    it('should render the app', (next) => {
-        const { container, getByText, getAllByText } = render(App)
+    it('should render the app', () => {
+        const { container, getAllByText } = render(App)
 
-        setTimeout(() => {
-            expect(container.getElementsByClassName('layout').length).toBe(1)
-            expect(container.getElementsByClassName('image-content').length).toBe(1)
-            expect(container.getElementsByClassName('bg-image').length).toBe(1)
-            expect(container.getElementsByClassName('title').length).toBe(1)
-            expect(container.getElementsByClassName('title__back').length).toBe(1)
-            expect(container.getElementsByClassName('title__front').length).toBe(1)
-            
-            expect(getAllByText('Hello World!').length).toBe(2)
-            expect(getByText('test'))
-            expect(getByText('test2'))
+        expect(container.getElementsByClassName('layout').length).toBe(1)
+        expect(container.getElementsByClassName('image-content').length).toBe(1)
+        expect(container.getElementsByClassName('bg-image').length).toBe(1)
+        expect(container.getElementsByClassName('title').length).toBe(1)
+        expect(container.getElementsByClassName('title__back').length).toBe(1)
+        expect(container.getElementsByClassName('title__front').length).toBe(1)
 
-            expect(container).toMatchSnapshot()
+        expect(getAllByText('Hello World!').length).toBe(2)
 
-            next()
-        }, 10)
+        expect(container).toMatchSnapshot()
     })
 })
